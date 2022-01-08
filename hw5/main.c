88a7d9937de9860dcf222866ecfb4cc96973ec36
Elad Sofer <elad.g.sofer@gmail.com>
2022-01-08 15:36:43 +0200

    hw5

---
+++ hw5/main.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>                                                              
+
+int main() {
+
+    return 0;
+}
--- os.c
+++
@@ -1,4 +0,0 @@
-//
-// Created by tomer on 10/12/2021.
-//
-
